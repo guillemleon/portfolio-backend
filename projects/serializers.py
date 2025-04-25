0e72d5132660002ca5ea9873e827b042e2f6aac0
guillemleon <guillem.leonf@gmail.com>
2025-04-25 15:13:37 +0200

    Return absolute image url not relative

--- projects/serializers.py
+++ projects/serializers.py
@@ -2,6 +2,21 @@ from rest_framework import serializers
 from .models import Project
 
 class ProjectSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+    image_card = serializers.SerializerMethodField()
+
     class Meta:
         model = Project
         fields = '__all__'
+
+    def get_image(self, obj):
+        if not obj.image:
+            return None
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.image.url)
+
+    def get_image_card(self, obj):
+        if not obj.image_card:
+            return None
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.image_card.url)
--- projects/views.py
+++ projects/views.py
@@ -5,3 +5,8 @@ from .serializers import ProjectSerializer
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all().order_by('-end_date')
     serializer_class = ProjectSerializer
+
+    def get_serializer_context(self):
+        ctx = super().get_serializer_context()
+        ctx['request'] = self.request
+        return ctx
